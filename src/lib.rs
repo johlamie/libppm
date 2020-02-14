1b1816f30f1524ceafe3345281f9192f06e009cb
Mnadege <msouici1@myges.fr>
2020-02-14 16:53:23 +0100

    Add Tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 // cargo new ppm --lib pour la création de la librairie
 // cargo fmt --> to indent code
-// cargo clippy -> 
+// cargo clippy ->
+// #![feature(test)] //
+//extern crate test; 
+
 use std::fmt;
 use std::fs::File; // pour la lecture du fichier
 use std::path::Path;
 use std::io::{self, BufReader};
-use std::io::prelude::*;
+
+
 
 struct Pixel {
     r: u8,
@@ -13,15 +17,9 @@ struct Pixel {
     b: u8,
 }
 
-struct Images {
-    buffer: Vec<Pixel>,
-    width: u32,
-    height: u32,
-}
-
 impl Pixel {
 
-    fn new(red: u8, green: u8, blue: u8) -> Pixel {
+    fn newPixel(red: u8, green: u8, blue: u8) -> Pixel {
         Pixel {
             r: red,
             g: green,
@@ -50,6 +48,7 @@ impl Pixel {
         self.r = 255 - self.r;
         self.g = 255 - self.g;
         self.b = 255 - self.b;
+
     }
 
     // Function to compare two pixel structures
@@ -63,7 +62,7 @@ impl Pixel {
 
         self.r = gray;
         self.g = gray;
-        self.b = gray;
+        self.b = gray; 
     }
 
 }
@@ -96,20 +95,31 @@ fn mysubstring(line: &String) -> Vec<u32> {
     return list;
 }
 
-fn new_with_file(filename: &Path) -> Image{
-    let mut file = File::open(filename).expect("Can't open file!"); // Ouverture du fichier
-    let buffer = BufReader::new(file); // Initialisation du buffer pour la lecture des lignes 
+struct Image {
+    buffer: Vec<Pixel>,
+    width: u32,
+    height: u32,
+}
 
-    // let mut buffer: Vec<Pixel> = Vec::new();
+fn new_with_file(filename: &Path) -> Option<Image>{
+    let mut img = Image {
+        width: 0,
+        height: 0,
+        buffer : Vec::new()
+    };
+    let mut buffer: Vec<Pixel> = Vec::new();
     let mut width: u32 = 0;
     let mut height: u32 = 0;
 
-    for (i, line) in buffer.lines().enumerate().by_ref() {// i réprésente la ligne (commence à 0) et line la valeur de la ligne
+    let mut file = File::open(filename).expect("Can't open file!"); // Ouverture du fichier
+    let buf = BufReader::new(file); // Initialisation du buffer pour la lecture des lignes 
+
+    for (i, line) in buf.lines().enumerate().by_ref() {// i réprésente la ligne (commence à 0) et line la valeur de la ligne
         // let l = line.unwrap(); // A SUPP
         // Vérifier que la premiere ligne nous dis que c'est un P3 avant de continuer
         if i == 0 { 
-            if &line.unwrap() != "P3" {
-                return None;
+            if (&line.unwrap() != "P3") {
+                println!("None");
             }
             // The second line is the dimensions of the picture.
         }else if (i == 1){
@@ -118,12 +128,108 @@ fn new_with_file(filename: &Path) -> Image{
             height = index[1];
         }else {
             // TODO
+            // ici que la variable buffer doit être remplie
             // le cas ou on n'est sur le maximum, un commentaire ou une ligne qui contient l'information qu'on recherche
         }
+    }
+   
+    return Some(Image::new(width, height, buffer));
+    // return new Images{content: Vec::new, height: 0, width: 0}
+}
+
+impl Image{
+
+    pub fn new(width: u32, height: u32, buffer: Vec<Pixel>) -> Image {
+        Image { width, height, buffer }
+    }
+
+    // Sauvegarde notre image dans un fichier ppm en respectant la structure
+    fn save(&self,filename: &Path) {
+            // On lui donne le type du fichier, le width, height et la valeur maximale
+            let mut str: String = format!("P3\n{} {}\n255\n", self.width, self.height);
+    
+            for p in self.buffer.iter() {
+                // On lui donne les pixels r,g et b
+                str = str + format!("{} {} {}\n", p.r, p.g, p.b).as_ref();
+            }
+            
+            // Création du fichier puis ecriture
+            let mut file = File::create(filename).expect("error");
+            file.write_all(str.as_ref());
 
     }
 
-    
-    
-    // return new Images{content: Vec::new, height: 0, width: 0}
+    // Inversion d'une image en utilisant la fonction inversion des pixels
+    fn invert(&mut self){
+        for pixel in self.buffer.iter_mut() {
+            pixel.invert();
+        }
+    }
+
+    // Mise en Noir & Blanc d'une image en utilisant la fonction grayscale des pixels
+    fn grayscale(&mut self) {
+        for pixel in self.buffer.iter_mut() {
+            pixel.grayscale();
+        }
+    }
+
+    // fn new_with_file_bin(filename: &Path ) -> Option<Image>{}
+
+
 }
+
+
+
+
+
+//----------------------------------------------Benchmark Tests---------------------------------------------------------------------------
+
+
+/*#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn testPixel() {
+        // let pixel = new Pixel(255,255,255);
+    }
+}
+*/
+
+#[cfg(test)]
+mod tests{
+  use super ::*;
+  #[test]
+/*  fn is_grayscale()
+  {
+      let pix= Pixel :: newPixel(200,30,50);
+      assert_eq!((60,17,23),pix.grayscale())
+  }
+  
+  fn it_invert()
+  {
+      let pix= Pixel :: newPixel(200,30,50);
+      //let pix_res= Pixel :: newPixel(55,225,205);
+      assert_eq!((55,225,205),pix.invert())
+          }
+*/
+
+  fn it_getGreen(){
+    let pix = Pixel :: newPixel(200,30,50);
+    assert_eq!((30), pix.getGreen())
+}
+  fn it_getBlue(){
+    let pix = Pixel :: newPixel(200,30,50);
+    assert_eq!((50), pix.getBlue())
+}
+  fn it_getRed(){
+    let pix = Pixel :: newPixel(200,30,50);
+    assert_eq!((200),pix.getRed())
+}
+
+      
+}
+
